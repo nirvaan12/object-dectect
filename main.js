de5f8c865920444a1a5f2fd1280524cba0bb4734
nirvaan12 <85714636+nirvaan12@users.noreply.github.com>
2022-05-24 19:54:24 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 img="";
-
+statuss="";
 function preload(){
     img=loadImage("dog_cat.jpg")
 }
@@ -7,6 +7,24 @@ function preload(){
 function setup(){
     canvas= createCanvas(640,420);
     canvas.center();
+    objectDetector= ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting Objects";
+
+}
+
+function modelLoaded(){
+    console.log("model is loaded");
+    statuss=true;
+  objectDetector.detect(img,gotResult);
+  
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    
 }
 
 function draw(){
@@ -16,4 +34,11 @@ function draw(){
     noFill();
     stroke("blue");
     rect(30,60,450,350);
+
+
+  fill("blue");
+    text("Cat",320,120);
+    noFill();
+    stroke("red");
+    rect(300,90,270,320);
 }
