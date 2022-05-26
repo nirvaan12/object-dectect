23c07161b90d70706e42fe7dde43b93d624a3a77
nirvaan12 <85714636+nirvaan12@users.noreply.github.com>
2022-05-26 19:44:08 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img="";
 statuss="";
+objects=[];
 function preload(){
     img=loadImage("dog_cat.jpg")
 }
@@ -22,23 +23,24 @@ function modelLoaded(){
 function gotResult(error,results){
     if(error){
         console.error(error);
+     
     }
     console.log(results);
-    
+    objects=results;
 }
 
 function draw(){
     image(img,0,0,640,420);
-    fill("red");
-    text("Dog",45,75);
-    noFill();
-    stroke("blue");
-    rect(30,60,450,350);
 
-
-  fill("blue");
-    text("Cat",320,120);
-    noFill();
-    stroke("red");
-    rect(300,90,270,320);
+  if(statuss != ""){
+      for(i=0; i<objects.length; i++){
+       document.getElementById("status").innerHTML="Status: Objects Dectected";
+      fill("green");
+      percent=floor(objects[i].confidence*100);
+      text(objects[i].label + " "+ percent + "%",objects[i].x+15,objects[i].y+15);
+      noFill();
+      stroke("blue");
+      rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+      }
+  }
 }
